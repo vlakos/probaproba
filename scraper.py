524fdecf900d4a2d9710c862649092a43722db07
vlakos <vkostic86@gmail.com>
2018-07-29 23:04:05 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,8 +5,8 @@
 
 import scraperwiki
 html = scraperwiki.scrape('http://inmo.ie/6022')
-print "Click on the ...more link to see the whole page"
-print html
+#print "Click on the ...more link to see the whole page"
+#print html
 
 # -----------------------------------------------------------------------------
 # 1. Parse the raw HTML to get the interesting bits - the part inside <td> tags.
@@ -20,10 +20,11 @@ print html
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 tds = root.cssselect('td') # get all the <td> tags
-print tds
+#print tds
 for td in tds:
-    print lxml.html.tostring(td) # the full HTML tag
-    print td.text                # just the text inside the HTML tag
+    print td.text_content()
+    #print lxml.html.tostring(td) # the full HTML tag
+    #print td.text                # just the text inside the HTML tag
 
 # -----------------------------------------------------------------------------
 # 2. Save the data in the ScraperWiki datastore.
@@ -33,12 +34,13 @@ for td in tds:
 # -----------------------------------------------------------------------------
 
 for td in tds:
-     record = { "td" : td.text } # column name and value
-     try:
-        scraperwiki.sqlite.save(["td"], record) # save the records one by one
-     except:
-        record = { "td" : "NO ENTRY" } # column name and value
-        scraperwiki.sqlite.save(["td"], record) # save the records one by one
+     record = { "td" : td.text_content() } # column name and value
+     scraperwiki.sqlite.save(["td"], record)
+     #try:
+       # scraperwiki.sqlite.save(["td"], record) # save the records one by one
+    # except:
+        #record = { "td" : "NO ENTRY" } # column name and value
+        #scraperwiki.sqlite.save(["td"], record) # save the records one by one
     
 # -----------------------------------------------------------------------------
 # Go back to the Tutorials page and continue to Tutorial 3 to learn about 
