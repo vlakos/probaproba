abdc57e3da5f0da70709aca40c56d7a99588f0a7
vlakos <vkostic86@gmail.com>
2018-07-29 17:26:25 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,41 +1,38 @@
-###############################################################################
-# START HERE: Tutorial 2: Basic scraping and saving to the data store.
-# Follow the actions listed in BLOCK CAPITALS below.
-###############################################################################
+####################################
+2 #STARTHERE:Tutorial2:Basicscrapingandsavingtothedatastore.
+ #FollowtheactionslistedinBLOCKCAPITALSbelow.
+ ####################################
 
-import scraperwiki
-html = scraperwiki.scrape('http://inmo.ie/6022')
-print "Click on the ...more link to see the whole page"
-print html
-
-# -----------------------------------------------------------------------------
-# 1. Parse the raw HTML to get the interesting bits - the part inside <td> tags.
-# -- UNCOMMENT THE 6 LINES BELOW (i.e. delete the # at the start of the lines)
-# -- CLICK THE 'RUN' BUTTON BELOW
-# Check the 'Console' tab again, and you'll see how we're extracting 
-# the HTML that was inside <td></td> tags.
-# We use lxml, which is a Python library especially for parsing html.
-# -----------------------------------------------------------------------------
-
-import lxml.html
-root = lxml.html.fromstring(html) # turn our HTML into an lxml object
-tds = root.cssselect('td') # get all the <td> tags
+ import scraperwiki
+ html = scraperwiki.scrape('http://inmo.ie/6022')
+ print "Clickonthe...morelinktoseethewholepage"
+ print html
+#---------------------------
+ #1.ParsetherawHTMLtogettheinterestingbits-thepartinside<tdtags.
+ #--UNCOMMENTTHE6LINESBELOW(i.e.deletethe#atthestartofthelines)
+ #--CLICKTHE'RUN'BUTTONBELOW
+ #Checkthe'Console'tabagain,andyou'llseehowwe'reextracting
+ #theHTMLthatwasinside<td</tdtags.
+ #Weuselxml,whichisaPythonlibraryespeciallyforparsinghtml.
+ #---------------------------
+    importlxml.html
+root = lxml.html.fromstring(html)#turnourHTMLintoanlxmlobject
+tds = root.cssselect('td')#getallthe<tdtags
 for td in tds:
-    print lxml.html.tostring(td) # the full HTML tag
-    print td.text                # just the text inside the HTML tag
+print lxml.html.tostring(td) # the full HTML tag
+print td.text # just the text inside the HTML tag
 
-# -----------------------------------------------------------------------------
-# 2. Save the data in the ScraperWiki datastore.
-# -- UNCOMMENT THE THREE LINES BELOW
-# -- CLICK THE 'RUN' BUTTON BELOW
-# Check the 'Data' tab - here you'll see the data saved in the ScraperWiki store. 
-# -----------------------------------------------------------------------------
-
+#---------------------------
+#2.SavethedataintheScraperWikidatastore.
+#--UNCOMMENTTHETHREELINESBELOW
+ #--CLICKTHE'RUN'BUTTONBELOW
+#Checkthe'Data'tab-hereyou'llseethedatasavedintheScraperWikistore.
+#---------------------------
 for td in tds:
-     record = { "td" : td.text } # column name and value
-     scraperwiki.sqlite.save(["td"], record) # save the records one by one
-    
-# -----------------------------------------------------------------------------
-# Go back to the Tutorials page and continue to Tutorial 3 to learn about 
-# more complex scraping methods.
-# -----------------------------------------------------------------------------
+ record = { "td" : td.text } # column name and value
+scraperwiki.sqlite.save(["td"], record) # save the records one by one
+
+#---------------------------
+ #GobacktotheTutorialspageandcontinuetoTutorial3tolearnabout
+#morecomplexscrapingmethods.
+#---------------------------
