43db8f66053066edca0c220968be20bd6630b983
vlakos <vkostic86@gmail.com>
2018-07-29 17:45:07 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,6 +20,7 @@ print html
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 tds = root.cssselect('td') # get all the <td> tags
+print tds
 for td in tds:
     print lxml.html.tostring(td) # the full HTML tag
     print td.text                # just the text inside the HTML tag
