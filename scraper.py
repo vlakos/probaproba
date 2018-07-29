15e47f514ca522fd0b4460730bac58c8bd2c389d
vlakos <vkostic86@gmail.com>
2018-07-29 17:15:05 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,34 +1,41 @@
 ###############################################################################
-# START HERE: Tutorial 1: Getting used to the ScraperWiki editing interface.
-# Follow the actions listed with -- BLOCK CAPITALS below.
+# START HERE: Tutorial 2: Basic scraping and saving to the data store.
+# Follow the actions listed in BLOCK CAPITALS below.
 ###############################################################################
 
-# -----------------------------------------------------------------------------
-# 1. Start by running a really simple Python script, just to make sure that 
-# everything is working OK.
-# -- CLICK THE 'RUN' BUTTON BELOW
-# You should see some numbers print in the 'Console' tab below. If it doesn't work, 
-# try reopening this page in a different browser - Chrome or the latest Firefox.
-# -----------------------------------------------------------------------------
-
-#for i in range(10):
-    #print "Hello", i
-
-# -----------------------------------------------------------------------------
-# 2. Next, try scraping an actual web page and getting some raw HTML.
-# -- UNCOMMENT THE THREE LINES BELOW (i.e. delete the # at the start of the lines)
-# -- CLICK THE 'RUN' BUTTON AGAIN 
-# You should see the raw HTML at the bottom of the 'Console' tab. 
-# Click on the 'more' link to see it all, and the 'Sources' tab to see our URL - 
-# you can click on the URL to see the original page. 
-# -----------------------------------------------------------------------------
-
 import scraperwiki
-html = scraperwiki.scrape('https://www.cins.rs/srpski')
-sausages = html
-print sausages
+html = scraperwiki.scrape('http://inmo.ie/6022')
+print "Click on the ...more link to see the whole page"
+print html
 
 # -----------------------------------------------------------------------------
-# In the next tutorial, you'll learn how to extract the useful parts
-# from the raw HTML page.
+# 1. Parse the raw HTML to get the interesting bits - the part inside <td> tags.
+# -- UNCOMMENT THE 6 LINES BELOW (i.e. delete the # at the start of the lines)
+# -- CLICK THE 'RUN' BUTTON BELOW
+# Check the 'Console' tab again, and you'll see how we're extracting 
+# the HTML that was inside <td></td> tags.
+# We use lxml, which is a Python library especially for parsing html.
+# -----------------------------------------------------------------------------
+
+import lxml.html
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td> tags
+for td in tds:
+    print lxml.html.tostring(td) # the full HTML tag
+    print td.text                # just the text inside the HTML tag
+
+# -----------------------------------------------------------------------------
+# 2. Save the data in the ScraperWiki datastore.
+# -- UNCOMMENT THE THREE LINES BELOW
+# -- CLICK THE 'RUN' BUTTON BELOW
+# Check the 'Data' tab - here you'll see the data saved in the ScraperWiki store. 
+# -----------------------------------------------------------------------------
+
+for td in tds:
+     record = { "td" : td.text } # column name and value
+     scraperwiki.sqlite.save(["td"], record) # save the records one by one
+    
+# -----------------------------------------------------------------------------
+# Go back to the Tutorials page and continue to Tutorial 3 to learn about 
+# more complex scraping methods.
 # -----------------------------------------------------------------------------
