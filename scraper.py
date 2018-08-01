fb4b8516d94a75cfc5ef399f2f1c3f1f8f60d8dc
vlakos <vkostic86@gmail.com>
2018-08-01 10:20:42 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@
 import scraperwiki
 html = scraperwiki.scrape('http://www.acas.rs/mere-javnog-objavljivanja-preporuke-za/')
 #print "Click on the ...more link to see the whole page"
-#print html
+print html
 
 # -----------------------------------------------------------------------------
 # 1. Parse the raw HTML to get the interesting bits - the part inside <td> tags.
@@ -17,10 +17,10 @@ html = scraperwiki.scrape('http://www.acas.rs/mere-javnog-objavljivanja-preporuk
 # We use lxml, which is a Python library especially for parsing html.
 # -----------------------------------------------------------------------------
 
-import lxml.html
-root = lxml.html.fromstring(html) # turn our HTML into an lxml object
-tds = root.cssselect('p a.href') # get all the <td> tags
-print tds
+#import lxml.html
+#root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+#tds = root.cssselect('p a.href') # get all the <td> tags
+#print tds
 #for tr in tds:
     #print tr.text_content()
     #print lxml.html.tostring(td) # the full HTML tag
